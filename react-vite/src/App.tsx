0d10ce895b8c264613645684872cfcbd691304e0
Felipe Gusmão <gusmao.dev@gmail.com>
2022-07-04 23:18:16 -0300

    Entendendo JSX

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -7,3 +7,5 @@ const App = () => {
 }
 
 export default App;
+
+// JSX nada mais é que um pseudo HTML, que são comandos HTML que são interpretados pelo compilador Babel e assim, transformados em JavaScript.
