3e6bd65db1d0b0bbff89b98a5c30e695592bd038
Felipe Gusmão <gusmao.dev@gmail.com>
2022-07-07 15:44:37 -0300

    Aula de eventos no JSX

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,20 +1,16 @@
-import Header from "./components/Header";
-import { Photo } from "./components/Photo";
-
 const App = () => {
-  let link: string = 'https://google.com.br';
+
+  const handleButtonClick = () => {
+
+    alert('O botão foi clicado! :D')
+
+  }
 
   return (
     <div>
-      <Header title="Exemplito" />
-      Link para o site <br/>
-      <a href={link}>Clique aqui!</a>
-
-      <Photo url="https://www.google.com.br/google.jpg"/>
+      <button onClick={ handleButtonClick }>Clique aqui!</button>
     </div>
   );
 }
 
 export default App;
-
-// JSX nada mais é que um pseudo HTML, que são comandos HTML que são interpretados pelo compilador Babel e assim, transformados em JavaScript.
