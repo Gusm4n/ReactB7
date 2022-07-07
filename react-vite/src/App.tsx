defba13e9e2f6ef8b2ed15b7fc35e47f639610f1
Felipe Gusmão <gusmao.dev@gmail.com>
2022-07-07 16:09:30 -0300

    Aula básica de state

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 const App = () => {
 
+const [numero, setNumero] = useState(0)
+const [name, setName] = useState('Felipe')
+
   const handleButtonClick = () => {
-
     alert('O botão foi clicado! :D')
+  }
 
+  const handleName = () => {
+    setName('Felipe Gusmão')
   }
 
   return (
     <div>
-      <button onClick={ handleButtonClick }>Clique aqui!</button>
+      Meu nome é: {name}.
+      <button onClick={ handleName }>Clique aqui!</button>
     </div>
   );
 }
