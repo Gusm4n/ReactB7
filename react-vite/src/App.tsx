8f3f7ff5cb17ef2cffee55bfefd17d5c4f5b0d59
Felipe Gusmão <gusmao.dev@gmail.com>
2022-07-10 17:53:58 -0300

    estilização dinamica

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [clicado, setClicado] = useState(false)
+
+  const handleClick = () => {
+    setClicado(!clicado)
+  }
+
   return (
     <div> 
-      <button style={{backgroundColor: '#ff0000', color:'#fff', border: 'none'}}>Clique aqui!</button>
+      <button 
+      onClick={handleClick}
+      style={
+        {backgroundColor: 
+        clicado ?
+        '#ff0000'
+        :
+        '#CCC000', 
+        color:'#fff', 
+        border: '0'}
+        }>Clique aqui!</button>
     </div>
     
   );
