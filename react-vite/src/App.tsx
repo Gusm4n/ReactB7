ec1472bc1c9cc98630f14a977516c4c6aa2f46a8
Felipe Gusmão <gusmao.dev@gmail.com>
2022-07-10 16:29:58 -0300

    Aula de renderização de conteudos React

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [show, setShow] = useState(false);
 
-  let list = [
-    'A3',
-    'A4',
-    'A5',
-    'Q3',
-    'TT'
-  ]
+  const handleClick = () => {
+    setShow( !show )
+  }
 
   return (
     <div> 
-      <h2>Lista de carros nacionais da AUDI</h2>
-      <ul>
-        {list.map((item, index)=>(
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <button onClick={handleClick}>{show ? 'Ocultar' : 'Mostrar'}</button>
+
+      {show && 
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi eaque sequi, tempore qui, libero ad temporibus alias hic placeat a inventore dolorum labore in ab nemo earum. Illo, culpa in.
+      </div>
+      }
     </div>
+    
   );
 }
 
