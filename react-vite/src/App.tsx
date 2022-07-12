a515744f8cb8d5c527ee2983876f4160b78d66ab
Felipe Gusmão <gusmao.dev@gmail.com>
2022-07-11 23:46:37 -0300

    Aula CSS modules

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,25 +1,13 @@
 import React, { useState } from "react";
+import { Botao } from "./components/Botao";
 
 const App = () => {
-  const [clicado, setClicado] = useState(false)
 
-  const handleClick = () => {
-    setClicado(!clicado)
   }
 
   return (
     <div> 
-      <button 
-      onClick={handleClick}
-      style={
-        {backgroundColor: 
-        clicado ?
-        '#ff0000'
-        :
-        '#CCC000', 
-        color:'#fff', 
-        border: '0'}
-        }>Clique aqui!</button>
+      <Botao />
     </div>
     
   );
