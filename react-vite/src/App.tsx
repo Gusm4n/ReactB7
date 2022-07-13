66a3ffbaf90c835536820f7bd8e92e413cf7ee78
Felipe Gusmão <gusmao.dev@gmail.com>
2022-07-12 23:24:34 -0300

    Aula de Styled Components

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import { Container } from "./components/Container";
+import { Botao } from "./components/Container";
+
 const App = () => {
 
 
   return(
     <div> 
-      
+      <Container>
+        Texto do componente
+        <Botao>
+          Clique aqui mesmo.
+        </Botao>
+      </Container>
     </div>
     
   );
